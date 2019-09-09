43938a9d01fdb02d9eb8ae51f474c96e946b2e4a
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-09-09 12:25:55 +0100

    Update theLargestWordFinder.js

--- theLargestWordFinder.js
+++ theLargestWordFinder.js
@@ -1,10 +1,10 @@
 
 function findLongestWordLength(str) {
-  var array = str.split(' ');
+  var array = str.split(' ');  // we split the string so we can loop through those individual words
   var maxLength = 0;
 
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].length > maxLength) {
+  for (var i = 0; i < array.length; i++) {  //looping through those words
+    if (array[i].length > maxLength) {  // we compare the length of each and every single word in the array until we find the longest
       maxLength = array[i].length;
     }
   }

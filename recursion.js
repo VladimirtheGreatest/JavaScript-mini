cd98a7c5e5ae73ae03afcadcc2bd424f8e891120
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-09-11 10:48:25 +0100

    Update recursion.js
    Recursion, "factorial function"

--- recursion.js
+++ recursion.js
@@ -1,6 +1,7 @@
 
 function factorialize(num) {
-  if (num === 0) { return 1; }
+  if (num === 0) { 
+    return 1; }
   let result = num * factorialize(num-1);
   console.log(result);
   return result;

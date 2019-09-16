f22ebccf62a98036b2b9564b13a7cda85940b043
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-09-16 08:11:31 +0100

    Add files via upload

---
+++ filteredArrayIndexOf.js
@@ -0,0 +1,14 @@
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for(let i=0; i < arr.length; i++){
+    if (arr[i].indexOf(elem)==-1){
+      newArr.push(arr[i])
+      console.log(newArr);
+     }
+  }
+  
+  return newArr;
+}
+
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 2, 9]], 3));

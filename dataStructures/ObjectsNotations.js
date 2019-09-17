76d9187ce5a88d2d567b60fc216d823e48a25977
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-09-17 11:25:17 +0100

    object data structures

---
+++ dataStructures/ObjectsNotations.js
@@ -0,0 +1,18 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+
+function checkInventory(scannedItem) {
+let item = foods[scannedItem]
+return item
+
+}
+
+
+console.log(checkInventory("apples"));

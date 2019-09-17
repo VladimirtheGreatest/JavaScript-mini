f8a11d39731a2cdfeaa3d431db784d8aa2c1f590
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-09-17 10:20:40 +0100

    adding data structures

---
+++ dataStructures/multiComplexArray.js
@@ -0,0 +1,16 @@
+let myNestedArray = [
+  ["unshift", false, 1, 2, 3, "complex", "nested"], //an array within an array, 2 levels of depth
+  [
+    ["deep", "shift", 6, 7, 1000, "method"],
+    ["concat", false, true, "spread", "array"] //2 arrays nested 3 levels deep
+  ],
+  [
+    [["deeper", 1327.98, "splice", "slice", "push"]], // an array nested 4 levels deep
+    [[["deepest", 1.3849, 7, "8.4876", "arbitrary", "depth"]]]  // an array nested 5 levels deep
+  ]
+];
+
+
+
+
+
--- splicer.js
+++
@@ -1,7 +0,0 @@
-function sumOfTen(arr) {
-  arr.splice(2, 1)
-  return arr.reduce((a, b) => a + b);
-}
-
-console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
-

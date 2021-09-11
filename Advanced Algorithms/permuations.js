75be7b5b0d67db18944d8d6e5c46c6c8092ef7d8
Vladimir <vladkodoktorov@gmail.com>
2021-09-11 13:31:18 +0100

    add permuations algo

--- Advanced Algorithms/permuations.js
+++ Advanced Algorithms/permuations.js
@@ -12,6 +12,7 @@ function permAlone(str) {
   
     // Function to swap variables' content.
     function swap(index1, index2) {
+        console.log('swapping', index1, index2);
       tmp = arr[index1];
       arr[index1] = arr[index2];
       arr[index2] = tmp;
@@ -19,19 +20,23 @@ function permAlone(str) {
   
     // Generate arrays of permutations using the algorithm.
     function generate(int) {
+        console.log('generating', int);
       if (int === 1) {
+        console.log('end of the generate');
         // Make sure to join the characters as we create  the permutation arrays
         permutations.push(arr.join(""));
+        console.log(permutations);
       } else {
+        console.log('before for loop');
         for (var i = 0; i != int; ++i) {
+            console.log(int - 1);
           generate(int - 1);
           swap(int % 2 ? 0 : i, int - 1);
         }
       }
     }
-  
     generate(arr.length);
-  
+
     // Filter the array of repeated permutations.
     var filtered = permutations.filter(x =>!x.match(regex));  
     // Return how many have no repetitions.

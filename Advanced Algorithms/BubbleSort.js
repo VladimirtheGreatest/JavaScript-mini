88dd140a407a6b0a5564dec98233c6aa499ac5d3
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2021-10-29 09:31:40 +0100

    Update BubbleSort.js
    fix typo

--- Advanced Algorithms/BubbleSort.js
+++ Advanced Algorithms/BubbleSort.js
@@ -5,7 +5,7 @@ function bubbleSort(array) {
         console.log(array[j], "array j");
         console.log(array[j+ 1], "array j +1");      
       if(array[j] > array[j + 1]){  //We compare each element to the one ahead of it, if the element before is smaller, we swap their places.
-        [array[j], array[j + 1]] = [array[j] + 1,array[j]]
+        [array[j], array[j + 1]] = [array[j+1],array[j]]
       }
     }
   }
